9c0b1fedefc6d73dcfae4d58d1601ad5189c488c
aayush <aayushsharma93107@gmail.com>
2026-02-28 14:01:45 +0530

    update

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,2 +1,14 @@
-name='rohit'
-print(name)
+import os
+import shutil
+
+path=input(" enter your path ")
+files=os.listdir(path)
+
+for file in files:
+    filename,extension=os.path.splitext(file)
+    extension=extension[1:]
+    if os.path.exists(path+'/'+extension):
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+    else:
+        os.makedirs(path+'/'+extension)
+        shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
